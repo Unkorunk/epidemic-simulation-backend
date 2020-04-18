a288493e249a8baba1578e667532d7298fe88c39
Юрий Михалев <mixalevuriy@gmail.com>
2020-04-18 22:50:10 +1000

    Add NCoV2019Parser with random countryId output

--- src/main/java/EpidemicSimulation.java
+++ src/main/java/EpidemicSimulation.java
@@ -1,10 +1,23 @@
+import parser.NCoV2019Parser;
+import parser.ParserData;
 import simulation.SimpleSimulation;
 import simulation.Simulation;
 
 public class EpidemicSimulation {
     public static void main(String[] args) {
-        Simulation simulation = new SimpleSimulation();
-        simulation.Tick(1);
-        System.out.println("Hello World");
+//        Simulation simulation = new SimpleSimulation();
+//        simulation.Tick(1);
+//        System.out.println("Hello World");
+        System.out.println("tesst");
+        ParserTest();
+
+
+    }
+
+    public static void ParserTest() {
+        NCoV2019Parser parser = new NCoV2019Parser();
+        ParserData res = parser.Parse();
+
+        System.out.println(res.data.get(0).toString());
     }
 }
--- src/main/java/parser/DayData.java
+++ src/main/java/parser/DayData.java
@@ -4,6 +4,11 @@ import java.time.LocalDate;
 import java.util.HashMap;
 
 public class DayData implements Comparable<DayData> {
+    public DayData() {
+        countries = new HashMap<Integer, CountryData>();
+        date = LocalDate.now();
+    }
+
     /// Key - country id
     public HashMap<Integer, CountryData> countries;
 
@@ -26,5 +31,17 @@ public class DayData implements Comparable<DayData> {
     @Override
     public int compareTo(DayData other) {
         return this.date.compareTo(other.date);
+    }
+
+    public String toString() {
+        var sb = new StringBuilder();
+        sb.append("Date: " + date.toString() + '\n');
+
+        for (var entry : countries.entrySet()) {
+            sb.append(entry.getValue());
+            sb.append('\n');
+        }
+
+        return sb.toString();
     }
 }
