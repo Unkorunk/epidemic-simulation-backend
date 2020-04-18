48f96914fd3c96e0d18ff7aa8963e00252a0f602
Юрий Михалев <mixalevuriy@gmail.com>
2020-04-18 19:39:13 +1000

    Add Parser abstract class

--- src/main/java/parser/ParserData.java
+++ src/main/java/parser/ParserData.java
@@ -1,12 +1,25 @@
 package parser;
 
-import java.util.Date;
-import java.util.Dictionary;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ParserData {
     public String source;
-    public Date startDate;
-    public Date endDate;
 
-    public Dictionary<Date, DayData> perDayData;
+    public HashMap<LocalDate, DayData> perDayData;
+
+    public void Merge(ParserData pd) {
+        for (var entry : pd.perDayData.entrySet()) {
+            if (this.perDayData.containsKey(entry.getKey())) {
+                this.perDayData.get(entry.getKey()).Merge(entry.getValue());
+            } else {
+                this.perDayData.put(entry.getKey(), entry.getValue());
+            }
+        }
+
+        if (pd.source != this.source) {
+            this.source = "undefined";
+        }
+    }
 }
