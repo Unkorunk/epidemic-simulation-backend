5e674590df5afa61bceaa87662102059fdcd7d32
Unkorunk <unkorunk@gmail.com>
2020-04-19 19:53:44 +1000

    Add simple Simulation

--- src/main/java/simulation/Simulation.java
+++ src/main/java/simulation/Simulation.java
@@ -3,12 +3,16 @@ package simulation;
 import parser.DayData;
 import parser.ParserData;
 
-import java.time.LocalDate;
+import java.util.ArrayList;
 
-public interface Simulation {
-    public void Tick(int duration);
-    public void Reset();
-    public void Reset(LocalDate startDate);
-    public void AddInfo(ParserData source);
-    public DayData GetInfo();
+public class Simulation {
+    public static ParserData Predict(DayData dayData, int duration) {
+        var parserData = new ParserData();
+        parserData.source = "Simulation";
+        parserData.data = new ArrayList<>();
+        parserData.data.add(dayData);
+
+
+        return parserData;
+    }
 }
